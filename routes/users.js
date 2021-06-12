8cfd6b4e19e83988c57493b6278761c9642bc82a
Abaliyoussef <y.abali_etu@enset-media.ac.ma>
2021-06-12 18:54:29 +0000

    Q.16 ajout des routes

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,19 @@ router.get('/', async function(req, res, next) { // localhost:3000/users
   res.send(  await usersRepo.getAllUsers() );
 });
 
+router.get('/', async function(req, res, next) { 
+  res.send(  await usersRepo.getUsers(req.query.off,req.query.lim) );
+});
+router.get('/:id', async function(req, res, next) { 
+  res.send(  await usersRepo.getUser(req.params.id) );
+});
+router.put('/', async function(req, res, next) { 
+  res.send(  await usersRepo.updateUser(req.body.user) );
+});
+router.post('/', async function(req, res, next) { 
+  res.send(  await usersRepo.addUser(req.body.user) );
+});
+router.delete('/:id', async function(req, res, next) { 
+  res.send(  await usersRepo.deleteUser(req.params.id) );
+});
 module.exports = router;
